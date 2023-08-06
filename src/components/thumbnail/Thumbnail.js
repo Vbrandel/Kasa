a065996ee6c4a6b928cbbc114c0650a1c18b7c95
Brandel <v.brandel76@gmail.com>
2023-08-06 18:55:31 +0200

    Finalisation du projet

--- src/components/thumbnail/Thumbnail.js
+++ src/components/thumbnail/Thumbnail.js
@@ -6,7 +6,9 @@
 import './thumbnail.scss'
 
 const Thumbnail = (props) => {
-    return <div  className="thumbnail" style={{background: `url(${props.image}) center`}}>
+    return <div  className="thumbnail" 
+    style={{background: `url(${props.image}) center`}}
+    alt="Appartement">
         <div className="background">
             <p>{props.title}</p>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,29 +25,12 @@ function Home() {
                 <Banner
                     image={image}
                     imageTitre="Chez vous, partout et ailleurs"
+                    alt="Paysage montagne"
                 />
                 <Gallery
                     appartements={appartements}
                 />
             </div>
-
-            {/* 
-            
-            IDEE : 
-            - Dans thumbnail: récupérer les photos directement dans le dossier du composant
-            - Mettre le style css pour chaque thumbnail
-            - Rappeler ensuite thumbnail dans gallery et faire le css de la gallery en flex ou grid
-
-            <Route path="/" component=Home />
-            <Route path="/:id" component=Appartment />
-            <Route path="/about" component=About />
-            <Route path="*" component=404 />
-
-            
-            */}
-
-
-
         </div>
     );
 }
